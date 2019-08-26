bd1a4ddcb0c779b2b01b4cdce9bd938a95424d08
Carlos M. Martinez <carlos@xt6.us>
2019-08-26 01:50:29 +0000

    bump module version

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 # 20170310 v1
 # 20170328 v 0.1.2
 # 20170329 v 0.1.3
-# 20190825 v 0.1.7
+# 20190825 v 0.1.9
 
 
 from setuptools import setup
 
-setup(name='lacniclabs',
-      version='0.1.8',
+setup(name='netdatalib',
+      version='0.1.9',
       description='cm2c python commons',
       url='https://github.com/cm2c-internet-measurements/rir-netdatalib',
       author='Carlos M. Martinez',
